d0c6f1e8eda956f925899cb778250b5bf7ff305b
novrion <128396601+novrion@users.noreply.github.com>
2023-12-01 17:12:46 +0100

    fix

--- README.md
+++
@@ -1,3 +0,0 @@
-# Beta Bot
-**Current version**: .\beta\
-**Download**: beta.zip
--- beta/compile.ps1
+++
@@ -1,1 +0,0 @@
-g++ -O2 -o run .\src\main.cpp .\src\bb.cpp .\src\eval.cpp .\src\moves.cpp .\src\search.cpp .\src\ui.cpp
